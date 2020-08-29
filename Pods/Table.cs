29a5961bfcb232076aa9a7b7b32411ec233cbd0f
Shawn Napora <17864647+shawnnapora@users.noreply.github.com>
2020-08-29 12:48:21 -0400

    Add some web junk

--- Pods/Table.cs
+++ Pods/Table.cs
@@ -10,11 +10,11 @@ namespace Pods
     public class Table
     {
         private List<Player> _players = new List<Player>();
-        private List<Card> _communityCards = new List<Card>();
+        private Card[] _communityCards = new Card[5];
         private Deck _deck;
 
         public ReadOnlyCollection<Player> Players => _players.AsReadOnly();
-        public ReadOnlyCollection<Card> CommunityCards => _communityCards.AsReadOnly();
+        public ReadOnlyCollection<Card> CommunityCards => _communityCards.ToList().AsReadOnly();
 
         public Table(int players)
         {
@@ -39,24 +39,30 @@ namespace Pods
         {
             foreach (Player player in _players)
             {
-                if (player.Card1 != null)
+                if (player.Card1 == null)
                 {
-                    continue;
+                    player.Card1 = _deck.Deal();
                 }
-                player.Card1 = _deck.Deal();
-                player.Card2 = _deck.Deal();
+
+                if (player.Card2 == null)
+                {
+                    player.Card2 = _deck.Deal();
+                }
             }
 
             for (int i = 0; i < 5; i++)
             {
-                _communityCards.Add(_deck.Deal());
+                if (_communityCards[i] == null)
+                {
+                    _communityCards[i] = _deck.Deal();
+                }
             }
         }
 
         public override string ToString()
         {
             string playerHands = String.Join(", ", _players.Select((v, i) => $"{i+1}:[{v}]"));
-            string communityCards = String.Join(" ", _communityCards);
+            string communityCards = String.Join(" ", (IEnumerable<Card>)_communityCards);
             return $"Players: {playerHands}, Community: {communityCards}";
         }
     }
